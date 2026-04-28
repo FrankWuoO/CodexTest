636ed5d8af7e4b1978469496d5ae8cde24fed6ea
Frank Wu <frankwu.cheng.en@gmail.com>
2026-04-29 06:06:54 +0800

    init

---
+++ CodexTest/CodexTest/CodexTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexTestApp.swift
+//  CodexTest
+//
+//  Created by cheng-en wu on 2026/4/29.
+//
+
+import SwiftUI
+
+@main
+struct CodexTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodexTest/CodexTestTests/CodexTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  CodexTestTests.swift
+//  CodexTestTests
+//
+//  Created by cheng-en wu on 2026/4/29.
+//
+
+import Testing
+
+struct CodexTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
